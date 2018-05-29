17fbcae5cdd8cf752ccc0046ad3b9c4ab362fa50
yangxiaoo <1270009836@qq.com>
2018-05-29 13:43:37 +0800

    labelme图片提取并处理

--- tool/abstract_picture.py
+++ tool/abstract_picture.py
@@ -1,13 +1,27 @@
-import json
 import os
-import os.path as osp
-import warnings
-import sys
-import re
-import shutil 
-import cv2  
-from skimage import io  
+import sys 
+import datetime
+import time
 # pip install opencv-python
+# pip install scikit-image
+# pip install numpy
+try:
+    import cv2 
+except:
+    os.system("pip install opencv-python")
+    import cv2
+
+try:
+    from skimage import io  
+except:
+    os.system("pip install scikit-image")
+    from skimage import io
+try:
+    import numpy as np
+except:
+    os.system("pip install numpy")
+    import numpy as np
+
 def get_filename(dirpath):
     file = []
     direction = []
@@ -26,36 +40,59 @@ def get_path(pic_file, out_dir, dirpath):
     file = os.path.splitext(basename)
     file_prefix = file[0]
     if file_prefix != prefix:
+        print("Skip: %s " % pic_file)
         return False
+    else:
+        print("Move %s" % pic_file)
+    dirname = os.path.dirname(pic_file)
     file_name = os.path.splitext(pic_file)
-    dir_path = file_name.split(file_prefix)[-1]
-    pwd = dir_path.split(os.sep)[-1:]
+    # print file_name
+    pwd = dirname.split(os.sep)[-1]
+    # print("pwd: %s" % pwd)
+    dir_path = dirname.split(pwd)[0]
+    # print dir_path
     dir_name = dir_path.split(dirpath)[-1]
-    dir_name = os.path.json(dir_name,out_dir)
+    dir_name = dir_name.strip('\\')
+    # print dir_name
+    dir_name = os.path.join(out_dir,dir_name)
+    # print dir_name
     try:
         os.makedirs(dir_name)
     except Exception,e:
-        print("%s" % e)
+        pass
+        # print("%s" % e)
     return dir_name,pwd
 
 def handle(pic,pwd,out_dir):
     out_pic = os.path.join(out_dir,str(pwd)+".png")
     img = io.imread(pic)
     img = img.astype(np.uint8)
-    cv2.imwrite(out_pic,img) 
+    cv2.imwrite(out_pic,img)
+    print("Saved in %s" % out_pic) 
 
 def main():
-    dirpath = raw_input("Please input direction of files:\n")
-    out_dir = "C:\\PIC_FILE"
+    print("************************")
+    print("*     LiuXinXinXin~    *")
+    print("*       2018-5-29      *")
+    print("************************")
+    dirpath = raw_input("\nInput direction:")
+    starttime = datetime.datetime.now()
+    # dirpath = "C:\\JSON_FILE"
+    out_dir = "C:\\PIC_FILE\\"
     file_list,dirname_list = get_filename(dirpath)
     i = 0
     for f in file_list:
         if not get_path(f,out_dir,dirpath):
             continue
-            print("error: %s" % i)
-            i += 1
+            print("Error:[ %s ]" % f)
+            i = i + 1
         else:
             dir_name,pwd = get_path(f,out_dir,dirpath)
+        i = i + 1
         handle(f,pwd,dir_name)
-        i =+ 1
-        print("%s\n" % i )
+    endtime = datetime.datetime.now()
+    expend = endtime - starttime
+    print("\n**************************")
+    print("Total: [ %s ]" % i )
+    print("Time: [ %s ]" % expend)
+main()

2d71012ae53b7fd11bcc3f9c2464bd78e2c2c34f
yangxiaoo <1270009836@qq.com>
2018-06-06 22:59:57 +0800

    ADT链表

--- C/程序/ADT链表/list.c
+++ C/程序/ADT链表/list.c
@@ -20,3 +20,84 @@ void InitializeList(List * plist)
 {
 	*plist = NULL;
 }
+
+bool IsEmpty(List * plist)
+{
+	if (*plist == NULL)
+		return true;
+	else
+		return false;
+}
+
+bool IsFull(List * plist)
+{
+	Node *pnew;
+
+	pnew = (Node *) malloc(seizeof(Node))
+	if (pnew == NULL)
+		return true;
+	else
+		return false;
+
+	free(pnew);
+}
+
+int ListItemCount(List *plist)
+{
+	Node *pnode = *plist;
+	int count = 0;
+
+	while (pnode != NULL)
+	{
+		count++;
+		pnode = pnode->next;
+	}
+
+	return count;
+}
+
+bool AddItem(List * plist, Item * item)
+{
+	Node * pnew;
+	Node * seek = *plist;
+
+	pnew = (Node *) malloc(sizeof(Node));
+	if (pnew == NULL)
+		return false;
+
+	pnew->item = item;
+	pnew->next = NULL;
+	if (seek == NULL)
+		*plist = pnew;
+	else
+	{
+		while (seek->next != NULL)
+			seek = seek->next;
+		seek->next = pnew;
+	}
+
+	return true;
+}
+
+void Traverse(List * plist, void(*pfun)(Item item))
+{
+	Node * pt = *plist;
+
+	while (pt != NULL)
+	{
+		(*pfun)(pt->item);
+		pt = pt->next;
+	}
+}
+
+void EmptyList(List * plist)
+{
+	Node * pn;
+
+	while (*plist != NULL)
+	{
+		pn = (*plist)->next;
+		free(*plist);
+		*plist = pn;
+	}
+}

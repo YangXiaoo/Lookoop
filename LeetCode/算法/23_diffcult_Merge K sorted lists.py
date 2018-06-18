6fd45b66f7cdf28fbf6310deb10df9063ec74643
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-06-18 19:07:53 +0800

    merge K sorted lists,超出内存限制？

---
+++ LeetCode/算法/23_diffcult_Merge K sorted lists.py
@@ -0,0 +1,75 @@
+'''
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+'''
+
+
+
+# 2018-6-18
+# Merge k Sorted Lists
+# 超出内存限制
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        res = []
+        lens = len(lists)
+        if lens == 0:
+            return lists
+        res = self.toLists(res,lists)
+        res = sorted(res)
+        h = l = ListNode(0)
+        for i in res:
+            cur = ListNode(i)
+            l.next = cur
+            l = cur
+        return h.next
+
+    def toLists(self,res,lists):
+        if len(lists) == 0:
+            return res
+        head = lists[0]
+        while head:
+            res.append(head.val)
+            head = head.next
+        return self.toLists(res,lists[1:])
+
+
+
+
+
+
+lis = [[1,2,4],[1,3,4],[7],[49],[73],[58],[30],[72],[44],[78],[23],[9],[40],[65],[92],[42],[87],[3],[27],[29],[40],[12],[3],[69],[9]]
+l = []
+for j in lis:
+    head = lists = ListNode(0)
+    for i in j:
+        cur = ListNode(i)
+        lists.next = cur
+        lists = cur
+    l.append(head.next)    
+
+
+test = Solution()
+res = test.mergeKLists(l)
+# show lists
+while res:
+    print(res,res.val,res.next)
+    res = res.next

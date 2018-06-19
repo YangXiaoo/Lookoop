cbf930e6c03a3b29c523046f5b492e2cfc29892d
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-06-19 21:08:29 +0800

    31,32,33,binary search,next permutation,,,,

---
+++ LeetCode/算法/33_medium_Search in Roated Array.py
@@ -0,0 +1,67 @@
+'''
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+0 1 2 4 5 6 7
+7 0 1 2 4 5 6
+6 7 0 1 2 4 5
+5 6 7 0 1 2 4
+4 5 6 7 0 1 2
+4 5 6 7 0 1 2
+2 4 5 6 7 0 1
+1 2 4 5 6 7 0
+如果中间数小于最右边的数则右边段是有序的
+若中间数大于最右边数则左边段是有序的
+'''
+
+# 2018-6-19
+# Search in Roated Array
+# binary search
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return -1
+        right = len(nums) - 1
+        left = 0
+        while left <= right:
+            middle = (left+right)//2
+            if nums[middle] == target:
+                return middle
+            if nums[left] <= nums[middle]:
+                if nums[left] <= target <= nums[middle]:
+                    right = middle - 1
+                else:
+                    left = middle + 1
+            else:
+                if nums[middle] <= target <= nums[right]:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+
+        return -1
+
+nums = [7,0,1,2,4,5,6]
+target = 0
+test = Solution()
+res = test.search(nums,target)
+print(res)
---
+++ LeetCode/算法/__32_diffcult_Longest valid Parenthese.py
@@ -0,0 +1,87 @@
+'''
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+
+Input: "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()"
+Example 2:
+
+Input: ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()"
+
+'''
+
+# 2018-6-19
+# Longest valid Parenthese
+# 暴力破解(超出时间限制)
+class Solution1:
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) == 0:
+            return 0
+        dicts = {")":"(","(":"#"}
+        r = []
+        res = []
+        index = []
+        res.append(0)
+        k = 0
+        i = 0
+        while i < len(s):
+            if dicts[s[i]] in r:
+                k += 2
+                r.pop()
+                index.pop()                
+            else:
+                index.append(i)
+                r.append(s[i])
+            res.append(k)
+            i += 1
+        res.pop(0)
+        index.append(i-1)
+        print(r,k,maxs,res,index)
+        if len(index) == 1:
+            return res[-1]
+        n = 0
+        node = []
+        node.append(res[index[0]]-res[0])
+        while n < len(index)-1:
+            node.append(res[index[n+1]]-res[index[n]])
+            n += 1
+        print(node)
+        return max(node)   
+
+
+class Solution2:
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """        
+        stack = [0]
+        longest = 0
+        
+        for c in s:
+            if c == "(":
+                stack.append(0)
+            else:
+                if len(stack) > 1:
+                    val = stack.pop()
+                    stack[-1] += val + 2
+                    longest = max(longest, stack[-1])
+                else:
+                    stack = [0]
+            # print(c,longest,stack)
+
+        return longest
+# s = ")()((()()(())"
+# s = ""
+s = ")))())(())()(("
+test = Solution2()
+res = test.longestValidParentheses(s)
+print(res)

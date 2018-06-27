b8ba5bf0da29af306f11de6ec2e22de796a2fb10
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-06-27 11:08:52 +0800

    word search,  Search in Rotated Sorted Array II,

--- LeetCode/算法/79_medium_Word Search.py
+++ LeetCode/算法/79_medium_Word Search.py
@@ -27,24 +27,24 @@ class Solution:
         :rtype: bool
         """
         for i in range(len(board)):
-        	for j in range(len(board[0])):
-        		if self.dfs(board,word,i,j,{},0):
-        			return True 
+            for j in range(len(board[0])):
+                if self.dfs(board,word,i,j,{},0):
+                    return True 
         return False 
 
     def dfs(self,board,word,i,j,visited,pos):
-    	if pos == len(word):
-    		return True 
-    	
-    	# 结束搜索条件
-    	if i < 0 or j < 0 or i == len(board) or j == len(board[0]) or visited.get((i,j)) or word[pos] != board[i][j]:
-    		return False
+        if pos == len(word):
+            return True 
+        
+        # 结束搜索条件
+        if i < 0 or j < 0 or i == len(board) or j == len(board[0]) or visited.get((i,j)) or word[pos] != board[i][j]:
+            return False
 
-    	visited[(i,j)] = True
-    	res = self.dfs(board,word,i+1,j,visited,pos+1) or self.dfs(board,word,i,j+1,visited,pos+1) or self.dfs(board,word,i-1,j,visited,pos+1) or self.dfs(board,word,i,j-1,visited,pos+1)
-    	visited[(i,j)] = False
+        visited[(i,j)] = True
+        res = self.dfs(board,word,i+1,j,visited,pos+1) or self.dfs(board,word,i,j+1,visited,pos+1) or self.dfs(board,word,i-1,j,visited,pos+1) or self.dfs(board,word,i,j-1,visited,pos+1)
+        visited[(i,j)] = False
 
-    	return res
+        return res
 
         
 
--- LeetCode/算法/81_medium_Search in Rotated Sorted Array.py
+++ LeetCode/算法/81_medium_Search in Rotated Sorted Array.py
@@ -1,21 +1,21 @@
-'''
+"""
 Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 
-(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+(i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
 
-You are given a target value to search. If found in the array return its index, otherwise return -1.
-
-You may assume no duplicate exists in the array.
-
-Your algorithm's runtime complexity must be in the order of O(log n).
+You are given a target value to search. If found in the array return true, otherwise return false.
 
 Example 1:
-Input: nums = [4,5,6,7,0,1,2], target = 0
-Output: 4
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
 
 Example 2:
-Input: nums = [4,5,6,7,0,1,2], target = 3
-Output: -1
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+
+Follow up:
+This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+Would this affect the run-time complexity? How and why?
 
 0 1 2 4 5 6 7
 7 0 1 2 4 5 6
@@ -25,43 +25,47 @@ Output: -1
 4 5 6 7 0 1 2
 2 4 5 6 7 0 1
 1 2 4 5 6 7 0
-如果中间数小于最右边的数则右边段是有序的
-若中间数大于最右边数则左边段是有序的
-'''
+"""
 
-# 2018-6-19
-# Search in Roated Array
-# binary search
+# 2018-6-27
+# Search in Rotated Sorted Array II
+# in case 1: nums[left] < nums[middle], handle left part
+# in case 2: nums[left] > nums[middle], handle right part
+# in case 3: nums[left] = nums[right], left += 1
 class Solution:
     def search(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
-        :rtype: int
+        :rtype: bool
         """
         if not nums:
-            return -1
+            return False
         right = len(nums) - 1
         left = 0
         while left <= right:
             middle = (left+right)//2
             if nums[middle] == target:
-                return middle
-            if nums[left] <= nums[middle]:
+                return True
+            if nums[left] < nums[middle]:
                 if nums[left] <= target <= nums[middle]:
                     right = middle - 1
                 else:
                     left = middle + 1
-            else:
+            elif nums[left] > nums[middle]:
                 if nums[middle] <= target <= nums[right]:
                     left = middle + 1
                 else:
                     right = middle - 1
+            else:
+                left += 1
 
-        return -1
+        return False
 
-nums = [1,3,1,1,1]
-target = 3
+
+# test
+nums = [1,1]
+target = 2
 test = Solution()
 res = test.search(nums,target)
 print(res)

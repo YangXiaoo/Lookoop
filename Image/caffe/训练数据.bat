27fff51cace93786c534bd100445f406705726ea
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-07-21 23:38:51 +0800

    数据标签

--- Image/caffe/训练数据.bat
+++ Image/caffe/训练数据.bat
@@ -1,50 +1,44 @@
 % 2018-7-21
 % 参考链接 	https://blog.csdn.net/nichengwuxiao/article/details/79134977 # 下载训练数据
 			https://blog.csdn.net/yanxiaopan/article/details/77485099
-% 下载好数据集分别为训练train和test文件， 复制到caffe/data/mydata/train 和 caffe/data/mydata/test中
-% 数据标签生成 python文件
+% 下载好的数据集分别为训练train和test文件， 将图片分别复制到caffe/data/mydata/train 和 caffe/data/mydata/test中
+%% 1
+% 数据标签生成
+% generator.py
+
 import os
- 
 if __name__ == "__main__":
-    test_dir = 'C:/software/caffe/caffe-master/data/mydata/test'
-    train_dir = 'C:/software/caffe/caffe-master/data/mydata/train'
-    test_fid = open("C:/software/caffe/caffe-master/data/mydata/test/test.txt","w")
-    train_fid = open("C:/software/caffe/caffe-master/data/mydata/train/train.txt","w")
-    test_files = os.listdir(test_dir)
-    train_files = os.listdir(train_dir)
-    index = 0
-    for ii, file in enumerate(test_files,1):
-        # print(file[0])
-        try:
-        # 333.jpg 0 图片路径与标签之间只能空一格
-            test_fid.write("{0}{1} {2}\n".format("test\\",file, int(file[0])-3))
-        except:
-            pass
-        index = index + 1
-        if index%100 == 0:
-            print("{0} images processed!".format(index))
-    # print("All images processed!")
-    test_fid.close()
 
-    index = 0
-    for ii, file in enumerate(train_files,1):
-        # print(file[0])
-        try:
-            train_fid.write("{0}{1} {2}\n".format("train\\",file, int(file[0])-3))
-        except:
-            pass
-        index = index + 1
-        if index%100 == 0:
-            print("{0} images processed!".format(index))
-    # print("All images processed!")
-    train_fid.close()
+    dirs = ["train", "test"] # 训练测试文件夹名称
+    data_dir = "C:/software/caffe/caffe-master/data/mydata/" # 依据自己创建路径修改
 
-% 在caffe下创建mybat/mydata 保存bat脚本文件
+    for d in dirs:
+        d_dir = data_dir + d
+        dir_pre = d + "\\"
+        file = d_dir + "/" + d + ".txt"
+        d_file = open(file, "w")
+        files = os.listdir(d_dir)
+        index = 0
+        for f in files:
+            try:
+            	# test\305.jpg 0 此处；图片路径与标签之间只空了一格
+                d_file.write("{0}{1} {2}\n".format(dir_pre, f, int(f[0])-3))
+            except:
+                pass
+            index += 1
+            if index % 100 == 0:
+                print("{0} processed".format(index))
+        d_file.close()
+    print("Successful")
+
+%% 2
+% 在caffe目录下创建mybat/mydata，以下bat脚本文件均保存在该目录下
 %数据转换 data_transfer.bat
 ..\..\Build\x64\Release\convert_imageset.exe --resize_height=32 --resize_width=32  --backend=leveldb ..\..\data\mydata\ ..\..\data\mydata\train\train.txt ..\..\data\mydata\trainldb
 ..\..\Build\x64\Release\convert_imageset.exe --resize_height=32 --resize_width=32  --backend=leveldb ..\..\data\mydata\ ..\..\data\mydata\test\test.txt ..\..\data\mydata\valldb
 pause
 
+%% 3
 % 均值生成 mean_generator.bat
 ..\..\Build\x64\Release\compute_image_mean.exe ..\..\data\mydata\trainldb --backend=leveldb ..\..\data\mydata\train_mean.binaryproto
 ..\..\Build\x64\Release\compute_image_mean.exe ..\..\data\mydata\valldb --backend=leveldb ..\..\data\mydata\val_mean.binaryproto

020c1cbbe9d384146ddcf2a08c7c3a2cb5fc2569
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-07-23 23:34:38 +0800

    137. Single Number II

---
+++ LeetCode/java/137_medium_Single_Number_II.java
@@ -0,0 +1,26 @@
+/**
+Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: [0,1,0,1,0,1,99]
+Output: 99
+*/
+
+// 2018-7-24
+// 137. Single Number II
+// https://www.cnblogs.com/yangrouchuan/p/5323327.html
+// https://www.cnblogs.com/ganganloveu/p/4110996.html
+class 137_medium_Single_Number_II {
+    public int singleNumber(int[] nums) {
+        
+    }
+}

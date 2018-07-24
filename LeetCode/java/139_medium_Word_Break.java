6d55bc2ce9b4fbd836d22b4f29a1684bebdfb2e7
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-07-24 21:33:58 +0800

    139. Word Break

--- LeetCode/java/139_medium_Word_Break.java
+++ LeetCode/java/139_medium_Word_Break.java
@@ -25,21 +25,68 @@ Output: false
 // 2018-7-24
 // 139. Word Break
 // https://blog.csdn.net/mine_song/article/details/72081998
+
+// dfs
 class 139_medium_Word_Break {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return dfs(0, 0, s, wordDict);
+        return dfs(0, 0, s, wordDict, 0);
     }
 
-    private boolean dfs(int start, int index, String s, List<String> dic) {
-        if (index == s.length()) return true;
+    private boolean dfs(int start, int index, String s, List<String> dic, int flag) {
+        if (index == s.length()) flag = 1;
 
-        for (int i = start; i < dic.length; i++) {
+        for (int i = start; i < dic.size(); i++) {
             String curStr = dic.get(i);
-            int lens = curStr.length;
-            String sub = s.subString(index, lens);
+            int lens = curStr.length();
+            if (index + lens > s.length()) continue;
+            String sub = s.substring(index, lens);
             if (sub != curStr) continue;
-            dfs(0, index + lens, s, dic);
+            index += lens;
+            dfs(0, index, s, dic, flag);
             index -= lens;
         }
+        System.out.println(index);
+        return flag == 1;
+    }
+}
+
+class Solution2 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (wordDict.contains(s)) return true;
+        List<Integer> queue = new ArrayList<>();
+        queue.offer(0);
+        Set<Integer> visit = new HashMap<>();
+        visit.add(0);
+
+        while (!queue.isEmpty) {
+            int curIndex = queue.poll();
+
+            for (int i = curIndex + 1; i <= s.length(); i++) {
+                if (visit.contains(i)) continue;
+
+                if (wordDict.contains(s.substring(curIndex, i ))) {
+                    if (i == s.length) return true;
+                    queue.offer(i);
+                    visit.add(i);
+                }
+            }
+        }
+
+        return false;
+    }
+}
+
+class Solution3 {
+    Set<String> map = new HashSet();
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if(wordDict.contains(s)) return true;
+        if(map.contains(s)) return false;
+        for(String word : wordDict){
+            if(s.startsWith(word)){
+                if(wordBreak(s.substring(word.length()), wordDict)) return true;
+            }
+        }
+        map.add(s);
+        return false;
     }
 }

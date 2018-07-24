29b980de41624ddaf4456428ef37eba3d9ede69c
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-07-24 19:46:36 +0800

    137. Single Number II

--- LeetCode/java/137_medium_Single_Number_II.java
+++ LeetCode/java/137_medium_Single_Number_II.java
@@ -15,12 +15,23 @@ Input: [0,1,0,1,0,1,99]
 Output: 99
 */
 
-// 2018-7-24
+// 2018-7-23
 // 137. Single Number II
 // https://www.cnblogs.com/yangrouchuan/p/5323327.html
 // https://www.cnblogs.com/ganganloveu/p/4110996.html
 class 137_medium_Single_Number_II {
     public int singleNumber(int[] nums) {
-        
+        hashMap<Integer, Integer> map = new hashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+        	if (!map.containsKey(nums[i])) {
+        		map.put(nums[i], 1);
+			} else {
+				map.put(nums[i], map.get(nums[i]) + 1);
+			}
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			if (map.get(nums[i]) != 3) return nums[i];
+		}
     }
 }
---
+++ LeetCode/java/138_medium_Copy_List_with_Random_Pointer.java
@@ -0,0 +1,24 @@
+/**
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+*/
+
+// 2018-7-24
+// 138. Copy List with Random Pointer
+// https://blog.csdn.net/edwal/article/details/46912979
+// https://blog.csdn.net/derrantcm/article/details/47745459
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class 138_medium_Copy_List_with_Random_Pointer {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        
+    }
+}

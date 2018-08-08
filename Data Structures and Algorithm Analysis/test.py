2846cd1f4d34faceb8b6d669c43f9ad41f198b61
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-08-08 23:12:09 +0800

    Red Black Tree

---
+++ Data Structures and Algorithm Analysis/test.py
@@ -0,0 +1,8 @@
+i = 3
+if i < 3:
+	i += 2
+else:
+	if i<1:
+		i += 2
+	i += 7
+print(i)
--- Data Structures and Algorithm Analysis/树/Red_Black_Tree.py
+++ Data Structures and Algorithm Analysis/树/Red_Black_Tree.py
@@ -1,4 +1,4 @@
-# 2018-8-7
+# 2018-8-7 - 2018-8-
 # Red Black Tree
 
 # Reference
@@ -7,6 +7,7 @@
 # https://en.wikipedia.org/wiki/Red%E2%80%93black_tree
 # C https://blog.csdn.net/weewqrer/article/details/51866488
 # python https://blog.csdn.net/jacke121/article/details/78178597
+# Java https://www.cnblogs.com/KingIceMou/p/6984138.html
 
 
 """
@@ -17,11 +18,15 @@
 5. Every path from a given node to any of its descendant NIL nodes contains the same number of black nodes.
 """
 
+# Global var
+BLACK = "black"
+RED = "red"
+
 # Define red-black tree
 class RedBlackNode(object):
 	def __init__(self, X=None):
-		self.value = X
-		self.color = "Black"
+		self.key = X
+		self.color = BLACK
 		self.right = None
 		self.left = None
 		self.parent = None
@@ -29,6 +34,9 @@ class RedBlackNode(object):
 
 class RedBlackTree(object):
 	def __init__(self):
+		"""
+		Initialize a red-black tree.
+		"""
 		self.root = RedBlackNode()
 		self.nil = None
 		self.root.left = self.nil
@@ -56,7 +64,7 @@ class RedBlackTree(object):
 			tmp_node = tmp_node.right
 		return tmp_node
 
-	def redBackTransplant(self, u, v):
+	def Transplant(self, u, v):
 		if u.parent == self.nil:
 			self.root = v
 		elif u == u.parent.left:
@@ -78,3 +86,130 @@ class RedBlackTree(object):
 		x may be any node in from x to y.It makes y the new root of subtree,with x as y's left child and y's left child as x's right child.
 		Both leftRotate and rightRotate run in O(1) time.
 		"""
+		y = x.right
+		x.right = y.left 
+		if y.left != self.nil:
+			y.left.parent = x
+
+		y.parent = x.parent
+		if x.parent == self.nil:
+			self.root = y
+		elif x == x.parent.left:
+			x.parent.left = y
+		elif x == x.parent.right:
+			x.parent.right = y
+
+		y.left = x
+		x.parent = y
+
+	def rightRotate(self, y):
+		"""
+				y
+			   / \
+			  x   r
+		     / \
+			a   b
+		Same as leftRotate
+		"""
+		x = y.left
+
+		y.left = x.right
+		if x.right != self.nil:
+			x.right.parent = y
+
+		x.parent = y.parent
+		if y.parent == self.nil:
+			self.root = x
+		elif y == y.parent.left:
+			y.parent.left = x
+		elif y == y.parent.right:
+			y.parent.right = x
+
+		x.right = y
+		y.parent = x
+
+	def insert(self, z):
+		"""
+		Insert a new node z, and color z red. 
+		Because color z red may cause a violation of one of the red-black properties, we call "insertFix(z)" to restore the red-black properties.
+		Insert a node in an n-node red-black tree run O(lgn) time.
+
+		The only properties that might be violated are property 2, which requires the root to be black, and property 4, which says that a red node cannot have a red child. Both possible violations are due to z being colored red. Property 2 is violated if z is the root, and property 4 is violated if z's parent is red.
+		"""
+		x = self.root
+		y = self.nil
+
+		while x != self.nil:
+			y = x
+			if z.key < x.key:
+				x = x.left
+			else:
+				x = x.right
+
+		z.p = y
+		if y.parent == self.nil:
+			self.root = y
+		elif z.key < y.key:
+			y.left = z
+		else:
+			y.right = z
+
+		z.color = RED
+		self.insertFix(z)
+
+	def insertFix(self, z):
+		"""
+		The while loop maintains the following three-part invariant at the start of each iteration of the loop.
+			a. Node z is red.
+			b. If z.parent is the root, then z.p is black.
+			c. If the tree violates any of the red-black properties, then it violates at most one of them, and the violation is of either property 4 or property 2.
+
+		There are three case:
+			a. z's uncle y is red.
+			b. z's uncle y is black and z is a right child.
+			c. z's uncle y is black and z is a left child.
+		"""
+		while z.parent.color == RED:
+			if z.parent == z.parent.parent.left:
+				y = z.parent.parent.right
+
+				# case a
+				if y.color == RED:
+					z.parent.color = BLACK 
+					y.color = BLACK
+					z.parent.parent.color = RED
+					z = z.parent.parent
+				else:
+					# case b
+					if z == z.parent.right:
+						z = z.parent
+						self.leftRotate(z)
+					# case c
+					z.parent.color = BLACK
+					z.parent.parent.color = RED
+					self.rightRotate(z.parent.parent)
+			else:
+				y = z.parent.parent.left
+
+				# case a
+				if y.color == RED:
+					z.parent.color = BLACK
+					y.color = BLACK
+					z.parent.parent.color = RED
+					z = z.parent.parent
+				else:
+					if z == z.parent.right：
+						z = z.parent
+						self.rightRotate(z)
+					z.parent.color = BLACK
+					z.parent.parent.color = RED
+					self.leftRotate(z.parent.parent)
+
+		self.root.color = BLACK
+
+
+
+
+
+
+

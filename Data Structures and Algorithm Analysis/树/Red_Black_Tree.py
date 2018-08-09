6efaf6d7711fae40852446ea92fd347420b986c0
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-08-09 23:40:12 +0800

    红黑树完成

--- Data Structures and Algorithm Analysis/树/Red_Black_Tree.py
+++ Data Structures and Algorithm Analysis/树/Red_Black_Tree.py
@@ -1,4 +1,4 @@
-# 2018-8-7 - 2018-8-
+# 2018-8-7 - 2018-8-9
 # Red Black Tree
 
 # Reference
@@ -24,7 +24,7 @@ RED = "red"
 
 # Define red-black tree
 class RedBlackNode(object):
-	def __init__(self, X=None):
+	def __init__(self, X):
 		self.key = X
 		self.color = BLACK
 		self.right = None
@@ -37,11 +37,25 @@ class RedBlackTree(object):
 		"""
 		Initialize a red-black tree.
 		"""
-		self.root = RedBlackNode()
+		self.root = None
 		self.nil = None
+		"""
 		self.root.left = self.nil
 		self.root.right = self.nil
 		self.root.parent = self.nil
+		"""
+	def inorder(self):
+		"""
+		Inorder traversal.
+		"""
+		node = self.root
+		self.traversal(node)
+
+	def traversal(self, node):
+		if node != self.nil:
+			self.traversal(node.left)
+			print("Inorder traversal--> color:",node.color,", key: ", node.key)
+			self.traversal(node.right)
 
 
 	def treeMaximum(self, node):
@@ -64,7 +78,10 @@ class RedBlackTree(object):
 			tmp_node = tmp_node.right
 		return tmp_node
 
-	def Transplant(self, u, v):
+	def transplant(self, u, v):
+		"""
+		Alternate v to u
+		"""
 		if u.parent == self.nil:
 			self.root = v
 		elif u == u.parent.left:
@@ -146,15 +163,18 @@ class RedBlackTree(object):
 			else:
 				x = x.right
 
-		z.p = y
-		if y.parent == self.nil:
-			self.root = y
+		if y == self.nil:
+			self.root = z
 		elif z.key < y.key:
 			y.left = z
 		else:
 			y.right = z
 
+		z.parent = y
+		z.left = self.nil
+		z.right = self.nil
 		z.color = RED
+		print("Insert node--> key: ", z.key, ", color: ", z.color)
 		self.insertFix(z)
 
 	def insertFix(self, z):
@@ -169,47 +189,161 @@ class RedBlackTree(object):
 			b. z's uncle y is black and z is a right child.
 			c. z's uncle y is black and z is a left child.
 		"""
-		while z.parent.color == RED:
-			if z.parent == z.parent.parent.left:
-				y = z.parent.parent.right
+		try:
+			while z.parent.color == RED:
+				if z.parent == z.parent.parent.left:
+					y = z.parent.parent.right
 
-				# case a
-				if y.color == RED:
-					z.parent.color = BLACK 
-					y.color = BLACK
-					z.parent.parent.color = RED
-					z = z.parent.parent
+					# case a
+					if y.color == RED:
+						z.parent.color = BLACK 
+						y.color = BLACK
+						z.parent.parent.color = RED
+						z = z.parent.parent
+					else:
+						# case b
+						if z == z.parent.right:
+							z = z.parent
+							self.leftRotate(z)
+						# case c
+						z.parent.color = BLACK
+						z.parent.parent.color = RED
+						self.rightRotate(z.parent.parent)
 				else:
-					# case b
-					if z == z.parent.right:
-						z = z.parent
-						self.leftRotate(z)
-					# case c
-					z.parent.color = BLACK
-					z.parent.parent.color = RED
-					self.rightRotate(z.parent.parent)
-			else:
-				y = z.parent.parent.left
+					y = z.parent.parent.left
 
-				# case a
-				if y.color == RED:
-					z.parent.color = BLACK
-					y.color = BLACK
-					z.parent.parent.color = RED
-					z = z.parent.parent
-				else:
-					if z == z.parent.right：
-						z = z.parent
-						self.rightRotate(z)
-					z.parent.color = BLACK
-					z.parent.parent.color = RED
-					self.leftRotate(z.parent.parent)
+					# case a
+					if y.color == RED:
+						z.parent.color = BLACK
+						y.color = BLACK
+						z.parent.parent.color = RED
+						z = z.parent.parent
+					else:
+						if z == z.parent.left:
+							z = z.parent
+							self.rightRotate(z)
+						z.parent.color = BLACK
+						z.parent.parent.color = RED
+						self.leftRotate(z.parent.parent)
+		except:
+			pass
 
 		self.root.color = BLACK
 
 
+	def delete(self, z):
+		"""
+		Delete node z, because color x may cause a violation of one of the red-black properties, we call "deleteFix(x)" to restore the red-black properties.
+		"""
+		y = z
+		y_original_color = y.color
+		if z.left == self.nil:
+			self.transplant(z, z.right)
+		elif z.right == self.nil:
+			self.transplant(z, z.left)
+		else:
+			y = self.treeMinimum(z.right)
+			y_original_color = y.color
+			x = y.right
+
+			if y.parent == z:
+				x.p = y
+			else:
+				self.transplant(y, x)
+				y.right = z.right
+				y.right.parent = y
+				self.transplant(z, y)
+				y.left = z.left
+				z.left.parent = y
+				y.color = z.color
+		print("delete--->", "key: ",z.key, "color : ",z.color)
+
+		"""
+		If y was red, the red-balck properties still hold when y is remove or move, for the following reasons:
+			a. No black-heights in the tree have changed.
+			b. No red node have been made adjacent.
+			c. Since y could not have been the root if it was red, the root remains black.
+
+		If y was black, three problems may arise, which the call of "deleteFix(x)" will remedy. 
+			a. If y had been the root and a red child of y becomes the new root, we have violated property 2.
+			b. If both x and x.parent are red, then we have violated property 4.
+			c. Moving y within thr tree causes any simple path that previously contained y to have one fewer black node. Thus, property 5 is now violated by any ancestor of y in the tree.
+		"""
+		if y_original_color == BLACK:
+			self.deleteFix(x)
 
 
+	def deleteFix(self, x):
+		"""
+		There are four case:
+			a. x's sibling w is red.(Will convert case a to case b, c, or d.)
+			b. x's sibling w is black, and both of w's children are balck.
+			c. x's sibling w is black, w's left child is red, and w's right child is black.(Will cause case d.)
+			d. x's sibling w is black, and w's right child is res. 
+		"""
+		while x != self.root and x.color == BLACK:
+			if x == x.parent.left:
+				w = x.parent.right
+				# case a
+				if w.color == RED:
+					w.color = BLACK
+					x.parent.color = RED
+					self.leftRotate(x.parent)
+					w = x.parent.right
+				# case b
+				if w.left.color == BLACK and w.right.color == BLACK:
+					w.color = RED
+					x = x.parent
+				else:
+					# case c (convert to case d)
+					if w.left.color == RED and w.right.color == BLACK:
+						w.color = RED
+						w.left.color = BLACK
+						self.rightRotate(w)
+						w = x.parent.right
+					# case d
+					w.color = x.parent.color
+					x.parent.color = BLACK
+					w.right.color = BLACK
+					self.leftRotate(x.parent)
+					x = self.root
+			else:
+				w = x.parent.left
+				if w.color == RED:
+					w.color = BLACK
+					x.parent.color = RED
+					self.rightRotate(x.parent)
+					w = x.parent.left
+				if w.left.color == BLACK and w.right.color == BLACK:
+					w.color = RED
+					x = x.parent
+				else:
+					if w.right.color == RED and w.left.color == BLACK:
+						w.color = RED
+						w.right.color = BLACK
+						self.leftRotate(w)
+						w = w.parent.left
+					w.color = x.parent.color
+					x.parent.color = BLACK
+					w.left.color = BLACK
+					self.rightRotate(x.parent)
+					x = self.root
 
+			x.color = BLACK
 
+def main():
+	"""
+	Test!
+	"""
+	test = RedBlackTree()
+	lits = [7,2,5,4,9,1,6,12,8]
+	for i in lits:
+		node = RedBlackNode(i)
+		test.insert(node)
+		del node
+	test.inorder()
+	test.delete(test.root)
+	test.inorder()
 
+if __name__ == "__main__":
+	main()

c3e3fec85459deec4ade71b1bfe6529dbad95e3d
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-08-30 22:23:15 +0800

    日语

---
+++ LeetCode/c++/587_(Geometry)_hard_Erect the Fence.cpp
@@ -0,0 +1,52 @@
+/* 
+There are some trees, where each tree is represented by (x,y) coordinate in a two-dimensional garden. Your job is to fence the entire garden using the minimum length of rope as it is expensive. The garden is well fenced only if all the trees are enclosed. Your task is to help find the coordinates of trees which are exactly located on the fence perimeter.
+
+Example 1:
+Input: [[1,1],[2,2],[2,0],[2,4],[3,3],[4,2]]
+Output: [[1,1],[2,0],[4,2],[3,3],[2,4]]
+Explanation:
+
+Example 2:
+Input: [[1,2],[2,2],[4,2]]
+Output: [[1,2],[2,2],[4,2]]
+Explanation:
+
+Even you only have trees in a line, you need to use rope to enclose them. 
+Note:
+
+All trees should be enclosed together. You cannot cut the rope to enclose trees that will separate them in more than one group.
+All input integers will range from 0 to 100.
+The garden has at least one tree.
+All coordinates are distinct.
+Input points have NO order. No order required for output.
+
+===================================================================
+*/
+// 2018-8-30
+// 587. Erect the Fence
+// https://leetcode.com/problems/erect-the-fence/description/
+
+// https://leetcode.com/problems/erect-the-fence/discuss/103299/Java-Solution-Convex-Hull-Algorithm-Gift-wrapping-aka-Jarvis-march
+
+
+
+
+// 使用 Andrew's Monotone Chain Algorithm 算法实现
+// 算法介绍： http://geomalgorithms.com/a10-_hull-1.html
+// 算法程序： http://www.algorithmist.com/index.php/Monotone_Chain_Convex_Hull.cpp
+
+/**
+ * Definition for a point.
+ * struct Point {
+ *     int x;
+ *     int y;
+ *     Point() : x(0), y(0) {}
+ *     Point(int a, int b) : x(a), y(b) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Point> outerTrees(vector<Point>& points) {
+        
+    }
+};

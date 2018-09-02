8466e2a9d3f00f295cad6ac937fa034698f03d6b
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-09-02 22:57:34 +0800

    Binary Indexed Tree

--- LeetCode/python/307_(Binary Indexed Tree)_hard_Range Sum Query - Mutable.py
+++ LeetCode/python/307_(Binary Indexed Tree)_hard_Range Sum Query - Mutable.py
@@ -1,23 +1,32 @@
-# 2018-9-2
-# Binary Indexed Tree
-# https://www.hackerearth.com/zh/practice/notes/binary-indexed-tree-made-easy-2/
-# 推荐看： https://www.cnblogs.com/whensean/p/6851018.html
-# 网页底部有数据结构总览表： https://en.wikipedia.org/wiki/Fenwick_tree
+'''
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
 
-# 来源于LeetCode
+The update(i, val) function modifies nums by updating the element at index i to val.
+
+Example:
+
+Given nums = [1, 3, 5]
+
+sumRange(0, 2) -> 9
+update(1, 2)
+sumRange(0, 2) -> 8
+Note:
+
+The array is only modifiable by the update function.
+You may assume the number of calls to update and sumRange function is distributed evenly.
+'''
+
+# 2018-9-2
 # 307. Range Sum Query - Mutable
 # Binary Indexed Tree
-
-# 关键在于 k -= (k & -k) 和 k += (k & -k), 前者用于update后者用于sum
 class NumArray:
+
     def __init__(self, nums):
         """
         :type nums: List[int]
         """
         self.nums = nums
         self.lens = len(nums)
-
-        # 初始化存储sum的数组
         self.BIT = [0] * (self.lens + 1)
         # map(update, range(self.lens), nums)
         for i in range(self.lens):
@@ -30,7 +39,6 @@ class NumArray:
 
     def update(self, i, val):
         """
-        update 操作一直都是遍历左侧
         :type i: int
         :type val: int
         :rtype: void
@@ -47,8 +55,6 @@ class NumArray:
 
     def sumRange(self, i, j):
         """
-        sum: 遍历右侧
-        上一层已经把左侧的值加上去了，而左侧的索引又是小于右侧，所以是合理的
         :type i: int
         :type j: int
         :rtype: int
@@ -73,5 +79,6 @@ obj = NumArray(nums)
 obj.update(1, 2)
 obj.update(1, 44)
 obj.update(2, 44)
+
 r1 = obj.sumRange(0, 2)
 print(r1,obj.nums)
--- LeetCode/python/test.py
+++ LeetCode/python/test.py
@@ -1,6 +1,91 @@
 # test
-t = [None] * 9
-t[2] = 3
-t = [[] for _ in range(len(t))]
-t[1].append(6)
-print(t)
+# About using the 2 functions:-
+# For update, pass index of location to be updated, input array, BIT, value to be added to original number
+# i.e. new value - original value
+# For getting sum of elements in range l to r,
+# Getsum returns sum of elements from beginning to index
+# Pass index, input array & BIT to function
+# getsum of l to r = getsum of r - getsum of (l-1)
+
+def update(index, a, tree, value):
+# index is index to be updated, a is input array / list, tree is BIT array, value is value to be added to original 
+# number at index location
+    add = value
+    n = len(a)
+    while index<n:
+        tree[index] += add
+        index = index + (index & (-index))
+
+def getsum(index, a, tree):
+# index is location upto which you want the sum of elements from beginning
+# tree is BIT[], a is input array / list
+    n = len(a)
+    ans  = 0
+    while(index>0):
+        ans += tree[index]
+        index = index - (index & (-index))
+    return ans
+
+#Get the user input
+# n = int( input("Number of Elements in array: "))
+# inputArray = list(map(int, raw_input("Elements in array: ").split()))
+inputArray = [1, 3, 5]
+n = 3
+inputArray.insert(0,0)                 # insert dummy node to have 1-based indexing
+
+#Initialise Binary Indexed Tree to 0's considering that input array is all 0's
+BIT = []
+for i in range(0, n):
+    BIT.append(0)
+
+# Now we will construct actual BIT
+# The 4th parameter is always an additional value which is to be added to element at index location
+# since we have considered input array as 0 earlier (while initialising BIT), for updating, we will pass actual 
+# value
+for i in range(1, n):
+    update(i, inputArray, BIT, inputArray[i])
+
+k = 15
+k = (k & -k)
+b = 4
+b = -b
+print(k)
+
+class NumArray(object):
+    def __init__(self, nums):
+        self.n = len(nums)
+        self.a, self.c = nums, [0] * (self.n + 1)
+        for i in range(self.n):
+            k = i + 1
+            while k <= self.n:
+                self.c[k] += nums[i]
+                k += (k & -k)
+        print(self.c)
+        
+
+    def update(self, i, val):
+        diff, self.a[i] = val - self.a[i], val
+        i += 1
+        while i <= self.n:
+            self.c[i] += diff
+            i += (i & -i)
+        print(self.c)
+
+    def sumRange(self, i, j):
+        res, j = 0, j + 1
+        while j:
+            res += self.c[j]
+            j -= (j & -j)
+        while i:
+            res -= self.c[i]
+            i -= (i & -i)
+        return res
+k = 3
+while k > 0:
+	print(k)
+	k -= (k & -k)
+print("________")
+m = 2
+while m < 7:
+	print(m)
+	m += (m & -m)

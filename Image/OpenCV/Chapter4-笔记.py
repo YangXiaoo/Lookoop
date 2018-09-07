10f768db3f23a5b8aa3c7bd3f60da112a4d58b12
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-09-07 11:56:35 +0800

    第四章笔记

--- Image/OpenCV/Chapter4-笔记.py
+++ Image/OpenCV/Chapter4-笔记.py
@@ -1,4 +1,4 @@
-# 2018-9-6
+# 2018-9-7
 # OpenCV3 计算机视觉 Python语言实现
 # Github : https://github.com/techfort/pycv
 # 英文教程： https://docs.opencv.org/3.2.0/d6/d00/tutorial_py_root.html
@@ -6,11 +6,83 @@
 # opencv中文教程: https://www.kancloud.cn/aollo/aolloopencv/272892
 # 第四章笔记
 import numpy as np
-import matplotlib.pyplot
+from matplotlib import pyplot as plt
 import scipy.special
 import os
 import cv2
 from scipy import ndimage
-# import cv # 已经被遗弃
 
 
+
+# GrabCut 进行前景检测
+img = cv2.imread('image/m_4.jpg')
+print(img.shape) # (500, 500, 3)
+mask = np.zeros(img.shape[:2],np.uint8) # 掩膜
+
+# 前景和背景
+bgdModel = np.zeros((1,65),np.float64)
+fgdModel = np.zeros((1,65),np.float64)
+print(bgdModel)  
+"""
+[[0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.
+  0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.
+  0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]]
+"""
+rect = (60,90,970,1360)
+
+# 5为迭代次数
+cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+
+# 满足条件输出0， 不满足则为1,
+# astype('uint8') 转换格式
+mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+img = img*mask2[:,:,np.newaxis] # np.newaxis = None
+
+plt.subplot(121), plt.imshow(img)
+plt.title("grabcut"), plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(cv2.cvtColor(cv2.imread('image/m_4.jpg'), cv2.COLOR_BGR2RGB))
+plt.title("original"), plt.xticks([]), plt.yticks([])
+plt.show()
+
+
+
+# 分水岭算法
+img = cv2.imread('image/m_2.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # 已经是灰度图
+ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU) # 将图像分为两个部分，黑色和白色
+
+cv2.imshow("thresh", thresh)
+
+# 去除噪音
+kernel = np.zeros((3,3), np.uint8)
+opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+
+
+# 通过 morphologyEx()变换之后对图像进行膨胀操作, 可以得到大部分都是北京的区域
+sure_bg = cv2.dilate(opening,kernel,iterations=3)
+# cv2.imshow("s", sure_bg)
+
+# 确定前景区域，使用阈值来确定哪些是前景区域
+dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+ret, sure_fg = cv2.threshold(dist_transform,0.5*dist_transform.max(),255,0)
+
+# 前景背景有重合区域，相减
+sure_fg = np.uint8(sure_fg)
+unknown = cv2.subtract(sure_bg,sure_fg)
+
+# Marker labelling
+ret, markers = cv2.connectedComponents(sure_fg)
+
+# 将背景区域加1， unknown区域设置为0
+markers = markers+1
+markers[unknown==255] = 0
+
+# 让水漫起来并把栅栏绘制成红色
+markers = cv2.watershed(img,markers)
+img[markers == -1] = [255,0,0]
+
+plt.imshow(img)
+plt.show()
+
+cv2.waitKey()
+cv2.destroyAllWindows()
--- Image/OpenCV/函数.py
+++ Image/OpenCV/函数.py
@@ -132,3 +132,10 @@ circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,120,
 
 
 ################################### 第四章函数 ###################################
+cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+# img 输入图像
+# mask 蒙板图像，确定前景区域，背景区域，不确定区域，可以设置为cv2.GC_BGD,cv2.GC_FGD,cv2.GC_PR_BGD,cv2.GC_PR_FGD，也可以输入0,1,2,3
+# rect 前景的矩形，格式为（x,y,w,h），分别为左上角坐标和宽度，高度
+# bdgModel, fgdModel 算法内部是用的数组，只需要创建两个大小为(1,65）np.float64的数组。
+# iterCount 迭代次数
+# mode cv2.GC_INIT_WITH_RECT 或 cv2.GC_INIT_WITH_MASK，使用矩阵模式还是蒙板模式。

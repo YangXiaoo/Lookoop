e0e4c056aa607838eec368fc12208e1785f78d28
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-09-14 21:07:23 +0800

    176. Second Highest Salary

---
+++ LeetCode/sql/176_easy_Second Highest Salary.sql
@@ -0,0 +1,36 @@
+/*
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+*/
+
+
+# 2018-9-14
+# 176. Second Highest Salary
+# https://leetcode.com/problems/second-highest-salary/description/
+
+/*
+Create table If Not Exists Employee (Id int, Salary int);
+Truncate table Employee;
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+*/
+
+# Write your MySQL query statement below
+SELECT DISTINCT Salary AS SecondHighestSalary  FROM Employee 
+ORDER BY Salary DESC 
+LIMIT 1 OFFSET 1;

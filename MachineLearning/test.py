017f8f106c252c7c1e30f9d37eb4482793b72ab1
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-12-18 20:29:22 +0800

    Update test.py

--- MachineLearning/test.py
+++ MachineLearning/test.py
@@ -29,4 +29,5 @@ import numpy as np
 import tensorflow as tf
 hello = tf.constant('Hello, TensorFlow!')  #初始化一个TensorFlow的常量
 sess = tf.Session()  #启动一个会话
-print(sess.run(hello))  
+print(sess.run(hello))
+ 

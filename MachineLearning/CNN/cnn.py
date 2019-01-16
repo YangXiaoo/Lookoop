00437ac053fe30456436ffcde570f8710b574dd8
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2019-01-16 13:00:23 +0800

    Update cnn.py

--- MachineLearning/CNN/cnn.py
+++ MachineLearning/CNN/cnn.py
@@ -1,6 +1,6 @@
 # coding:UTF-8
 # 2018-11-12 ~ 11-16
-# CNN
+# CNN 
 # 卷积网络知识 https://blog.csdn.net/luanpeng825485697/article/details/79009241
 # 代码：  https://blog.csdn.net/luanpeng825485697/article/details/79088938
 # 吴恩达卷积参数符号表示：https://blog.csdn.net/ice_actor/article/details/78648780

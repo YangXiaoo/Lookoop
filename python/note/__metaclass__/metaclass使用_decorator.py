7a5c05343e513bfbc914465b4b5479e662bbdda9
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2019-02-01 22:30:39 +0800

    update...

--- python/note/__metaclass__/metaclass使用_decorator.py
+++ python/note/__metaclass__/metaclass使用_decorator.py
@@ -1,15 +1,14 @@
 from types import FunctionType  
   
 def login_required(func):  
-    print 'login check logic here'  
+    print( 'login check logic here' ) 
     return func  
   
   
 class LoginDecorator(type):  
     def __new__(cls, name, bases, dct):  
         for name, value in dct.iteritems():  
-            if name not in ('__metaclass__', '__init__', '__module__') and\  
-                type(value) == FunctionType:  
+            if name not in ('__metaclass__', '__init__', '__module__') and type(value) == FunctionType:  
                 value = login_required(value)  
   
             dct[name] = value  
@@ -20,9 +19,12 @@ class Operation(object):
     __metaclass__ = LoginDecorator  
   
     def delete(self, x):  
-        print 'deleted %s' % str(x)  
+        print( 'deleted %s' % str(x) ) 
   
   
 def main():  
     op = Operation()  
     op.delete('test')  
+
+if __name__ == '__main__':
+    main()

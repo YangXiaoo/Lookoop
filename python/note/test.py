c218238ebd44d7a5bcb4396b67e37dffd27066c9
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2019-02-01 20:24:12 +0800

    Update test.py

--- python/note/test.py
+++ python/note/test.py
@@ -3,7 +3,11 @@ import tool_test_yauno
 
 def fooo(arg=None,
 		 a=None):
-	print("pass")
+	for i in range(1000000000):
+		pass
+	print("end")
 
 if __name__ == '__main__':
-	fooo()
+	import cProfile
+	cProfile.run("fooo()")
+	

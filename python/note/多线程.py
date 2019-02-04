677b3bb0fe06ea4d301363d0df73e903b042390f
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2019-02-05 00:42:48 +0800

    update...

--- python/note/多线程.py
+++
@@ -1,4 +0,0 @@
-# 2018-8-22
-# 多线程
-# 音乐电影播放例子： https://www.cnblogs.com/yeayee/p/4952022.html
-# 廖雪峰： https://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000/0013868323401155ceb3db1e2044f80b974b469eb06cb43000

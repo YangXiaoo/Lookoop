f5f6ec387640a8109cd14e7b5601d0a591d63498
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2019-03-01 10:52:08 +0800

    upload....

---
+++ tool/ImgSeg/ImgSeg/MLCore.cc
@@ -0,0 +1,57 @@
+// MLCore.cc
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include <cmath>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+using namespace cv;
+using namespace std;
+
+#include "MLCore.h"
+
+/*********************** Softmax***********************/
+
+// 训练
+Mat Softmax::train() {
+	int m = _feature.rows, n = _feature.cols;
+	_weight = Mat::ones(Size(n, k), CV_32FC1);
+	int it = 0;
+	float pre = 1e5, gap = 1, error_rate;
+	Mat y, row_sum = Mat::zeros(Size(m, 1), CV_32FC1);
+	Mat _row_sum;
+	while (it <= _max_iter && gap > _min_error) {
+		exp(_feature * _weight, y);
+		if (it % 500 == 0) {
+			error_rate = cal_error(y);
+			gap = abs(error_rate - pre);
+			pre = error_rate;
+			cout << "iteration: " << it
+				 << ", error rate: " << error_rate
+				 << ", gap: " << gap << endl;
+		}
+		reduce(-y, row_sum, 1, CV_REDUCE_SUM);
+		repeat(row_sum, k, 1, _row_sum);
+		y = y / _row_sum;
+		for (int x = 0; x != m; ++x) {
+			y.at(x, _labels.at<int>(x, 0)) += 1;
+		}
+		_weight += (_alpha / m) * _feature.t() * y;
+		++it;
+	}
+	return _weight
+}
+
+// 计算误差
+float Softmax::cal_error(Mat err) {
+
+}
+
+// 预测
+int Softmax::predict(Mat &test_data) {
+
+}
+
--- tool/ImgSeg/ImgSeg/MLCore.h
+++ tool/ImgSeg/ImgSeg/MLCore.h
@@ -1,10 +1,4 @@
 // MLCore.hpp
-// 使用策略模式编写机器学习类
-
-#include <vector>
-#include <string>
-#include <map>
-#include <functional>
 
 // OpenCV
 #include "opencv2/core/core.hpp"
@@ -14,46 +8,27 @@ using namespace cv;
 #ifndef ML_CORE
 #define ML_CORE
 
-#define free_ptr(p) if(p) delete p; o = NULL;
-
-// 抽象基
-class Model {
- public:
- 	virtual ~Model(){};
- 	virtual Mat train() = 0;
- 	virtual int predict() = 0;
- 	virtual float cal_error() = 0;
-};
+#define free_ptr(p) if(p) delete p; p = NULL;
 
 // softmax多分类
-class Softmax : public Model {
+class Softmax {
  public:
+ 	Softmax() = default;
+ 	Softmax(const Mat &feature, 
+ 	   		const Mat labels, int k, 
+ 	   		long long max_iter, float alpha=0.1, float min_error=0.0001) 
+ 		    : _feature(feature), _labels(labels), _max_iter(max_iter), _alpha(alpha), _min_error(min_error) {};
  	Mat train();
- 	int predict();
- 	float cal_error();
+ 	int predict(Mat &test_data);
+ 	float cal_error(Mat err);
  private:
  	// 权重
  	Mat weight;
+ 	Mat _feature;
+ 	Mat _labels;
+ 	long long _max_iter;
+ 	float _min_error; 
+ 	float _alpha;
 };
 
-
-
-class ML {
- public:
- 	ML() = default;
- 	// softmax构造
- 	ML(const std::string &ml_name, 
- 	   const Mat &feature, 
- 	   const Mat labels, int k, 
- 	   long long max_iter, float alpha=0.1) noexcept { _choose_model(ml_name); };
- 	// 训练
- 	Mat train() { _model->train(); };
- 	// 预测
- 	int predict() {  _model->train(); };
- 	// 根据名字选择接口
- 	void _choose_model(const std::string &ml_name);
- private:
- 	Model* _model;
- 	std::string _model_name;
-};
 #endif

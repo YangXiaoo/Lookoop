5954eb9e553255a341e74ca36b7638d896babff0
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2019-05-31 20:31:16 +0800

    Update notebook.java

--- java/笔记/notebook.java
+++ java/笔记/notebook.java
@@ -786,14 +786,14 @@
 	// https://www.cnblogs.com/lemon-flm/p/7877898.html
 	// 非阻塞
 	  
-	ConcurrentLinkedQueue
+	ConcurrentLinkedQueue	// 基于链节点的无界线程安全队列，遵循FIFO
 	// 双端列队
 	Deque
 	// 阻塞列队
 	BlockingQueue为接口
 	ArrayBlockingQueue // 一个由数组支持的有界队列。在构造时需要指定容量， 并可以选择是否需要公平性，如果公平参数被设置true，等待时间最长的线程会优先得到处理（其实就是通过将ReentrantLock设置为true来 达到这种公平性的：即等待时间最长的线程会先操作）。通常，公平性会使你在性能上付出代价，只有在的确非常需要的时候再使用它。它是基于数组的阻塞循环队 列，此队列按 FIFO（先进先出）原则对元素进行排序。
-　　LinkedBlockingQueue //一个由链接节点支持的可选有界队列。容量是没有上限的（说的不准确，在不指定时容量为Integer.MAX_VALUE，不要然的话在put时怎么会受阻呢），但是也可以选择指定其最大容量，它是基于链表的队列，此队列按 FIFO（先进先出）排序元素。
-　　PriorityBlockingQueue //一个由优先级堆支持的无界优先级队列。是一个带优先级的 队列，而不是先进先出队列。元素按优先级顺序被移除，该队列也没有上限（看了一下源码，PriorityBlockingQueue是对 PriorityQueue的再次包装，是基于堆数据结构的，而PriorityQueue是没有容量限制的，与ArrayList一样，所以在优先阻塞 队列上put时是不会受阻的。虽然此队列逻辑上是无界的，但是由于资源被耗尽，所以试图执行添加操作可能会导致 OutOfMemoryError），但是如果队列为空，那么取元素的操作take就会阻塞，所以它的检索操作take是受阻的。另外，往入该队列中的元 素要具有比较能力。
+　　LinkedBlockingQueue //一个由链接节点支持的可选有界队列，不允许有null值。容量是没有上限的，但是也可以选择指定其最大容量，它是基于链表的队列，此队列按 FIFO（先进先出）排序元素。
+　　PriorityBlockingQueue //一个由优先级堆支持的无界优先级队列。是一个带优先级的队列，而不是先进先出队列。不允许null值，入队和出队复杂度为O(log(n))
 　　DelayQueue //一个由优先级堆支持的、基于时间的调度队列。是一个存放Delayed 元素的无界阻塞队列，只有在延迟期满时才能从中提取元素。该队列的头部是延迟期满后保存时间最长的 Delayed 元素。如果延迟都还没有期满，则队列没有头部，并且poll将返回null。当一个元素的 getDelay(TimeUnit.NANOSECONDS) 方法返回一个小于或等于零的值时，则出现期满，poll就以移除这个元素了。此队列不允许使用 null 元素。
 　　SynchronousQueue //一个利用 BlockingQueue 接口的简单聚集（rendezvous）机制。
 	BlockingQueue<String> queue = new ArrayBlockingQueue<String>();

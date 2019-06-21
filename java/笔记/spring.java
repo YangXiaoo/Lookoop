5953a298ba47a67ed700c762c96cdd6b6b1b209b
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2019-06-21 22:55:28 +0800

    Update spring.java

--- java/笔记/spring.java
+++ java/笔记/spring.java
@@ -45,3 +45,5 @@ MyBaits配置
 // ################
 Session // https://blog.csdn.net/z_xuewen/article/details/77985897
 Cookie // https://blog.csdn.net/u011848397/article/details/52201339
+
+@ContextConfiguration: 加载配置文件

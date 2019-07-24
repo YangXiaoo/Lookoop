bcc7b4f330eb73e178dba3fe4cbb42886c4beb4b
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2019-07-24 18:28:14 +0800

    update...

--- interview/README_new.md
+++ interview/README_new.md
@@ -1,66 +1,59 @@
 ---
-> ## 框架
-
-### Maven特点？
+## 框架
+### Maven特点? 
 ### Solr是什么?
 ### Thymeleaf是什么?
 ### 什么是webService?
 ### Tomcat结构以及类加载流程?
 ### thinkPHP处理流程?
 ---
-> ## JSP
-
+## JSP
 ### JSP有哪些动作?
 ### 会话跟踪技术?
 ### JSP内置九大类型?
 ---
-> ## MyBatis
-
-### MyBatis中#{}和${}的区别？
+## MyBatis
+### MyBatis中#{}和${}的区别? 
 ### PreparedStatement与Statement区别?
 ### Mybatis缓存?
 ---
-> ## spring
-
-### sprig有哪些特点？
+## spring
+### sprig有哪些特点? 
 ### spring核心?
 ### spring有几种注入方式?
-### 什么是IOC/DI,什么是AOP？
+### 什么是IOC/DI,什么是AOP? 
 ### spring事物的传播级别?
-### spring中Scope作用域？
-### springMVC路由怎么写？
+### spring中Scope作用域? 
+### springMVC路由怎么写? 
 ### springMVC如何接受ajax?
-### springMVC用到了那个核心servlet？
+### springMVC用到了那个核心servlet? 
 ### springMVC核心控制器是什么? 请求流程?
 ### SpringBoot优点?
-### 关于加@Transactional注解的方法之间调用，事物是否生效?
+### 关于加@Transactional注解的方法之间调用, 事物是否生效?
 ### Bean的生命周期?
 ### Spring用了哪些设计模式?
 ---
-> ## 其他
-
+## 其他
 ### 对软件测试的理解?
 ### 一个完整的测试应有哪些阶段组成?
 ### 单元测试怎么实现?
-### 虚拟化技术？
-### 赛马，有25匹马，每次只能5匹马比赛，比赛只能得到5匹马之间的快慢，而不是速度。请问，最少比赛多少次，才能获得最快的前3匹马?
+### 虚拟化技术? 
+### 赛马, 有25匹马, 每次只能5匹马比赛, 比赛只能得到5匹马之间的快慢, 而不是速度. 请问, 最少比赛多少次, 才能获得最快的前3匹马?
 
 
 ---
-> ## 网络应用类
-
+## 网络应用类
 ### 如何实现单点登录?
 ### 什么是servlet?
 ### servlet生命周期?
 ### servlet处理流程?
 ### 服务端包含?
 ### 什么是servlet链?
-### servlet安全性？
+### servlet安全性? 
 ### SSH协议?什么是跳板机?
 ---
-> ## Python
-
-### Pyton垃圾回收策略？
+## Python
+### Pyton垃圾回收策略? 
 ### MVC与MVT模式?
 ### 什么是wsgi?uwsgi?
 ### Django请求生命周期?
@@ -68,8 +61,7 @@
 ### Django模板中自定义filter与simple_tag区别?
 ### Flask与Django区别?
 ---
-> ## Linux
-
+## Linux
 ### Linux文件和目录操作
 ### 创建文件, 查看文件
 ### 创建目录
@@ -78,6 +70,5 @@
 ### 压缩数据
 ### 结束进程
 
-> ## 生活
-
+## 生活
 ### 职业规划?
---
+++ tool/文件处理/replaceChineseCharacer.py
@@ -0,0 +1,79 @@
+# coding: utf-8
+"""
+转换文件中的中文格式
+"""
+import os
+class Exg(object):
+	def __init__(self, oldFilePath, outputFilePath=None, 
+				 oldList=None, newList=None):
+		"""初始化
+		@param oldFilePath 必须填写
+		"""
+		self.filePath = oldFilePath
+		self.outputFilePath = outputFilePath
+		self.addRegx(oldList, newList)
+
+
+	def addRegx(self, oldList=None, newList=None):
+		"""添加规则
+		添加列表规则, 需对应： ["，", "？", "："], [", ", "? ", ": "]
+		or 添加单个规则, 不用列表: addRegx("，", ", ")
+		@param oldList 待替换的字符串
+		@param newList 新字符串
+
+		@return None
+		"""
+		if oldList == None or newList == None:
+			return
+
+		if not isinstance(newList, list):
+			self.oldList = [oldList]
+			self.newList = [newList]
+		else:
+			self.oldList = oldList
+			self.newList = newList
+
+	def writeInto(self, outputFilePath=None):
+		if self.outputFilePath == None and outputFilePath == None:
+			assert False, "确定输出路径"
+			return 
+
+		self.outputFilePath = outputFilePath
+
+		newData = []
+		with open(self.filePath, encoding="utf-8") as f:
+			data = f.readlines()
+			for line in data:
+				for regxIndex, regx in enumerate(self.newList):
+					line = line.replace(self.oldList[regxIndex], regx)
+				newData.append(line)
+
+		newFile = open(self.outputFilePath, "w", encoding="utf-8")
+		newFile.write("".join(newData))
+		newFile.close()
+
+
+def mkfile(filePath, midffix=""):
+	"""创建新文件路径，在文件名的后面,格式前面添加新的字符
+	@param filePath 原文件路径
+	@param midffix 文件名添加的字符
+	
+	@return 新文件路径
+	"""
+
+	fileDirName = os.path.dirname(filePath)
+	fileBaseName = os.path.basename(filePath)
+	filePreffix = fileBaseName.split(".")[0]
+	fileSuffix = fileBaseName.split(".")[-1]
+	outputFileName = os.path.join(fileDirName, filePreffix + midffix + "." + fileSuffix)
+
+	return outputFileName
+
+if __name__ == '__main__':
+	filePath = r"C:\Study\github\Lookoops\interview\java.md"
+	newFilePath = mkfile(filePath, "_new____")
+	re = Exg(filePath)
+	re.addRegx(["，", "？", "："], [", ", "? ", ": "])
+	re.writeInto(newFilePath)
+
+	# replace(filePath)

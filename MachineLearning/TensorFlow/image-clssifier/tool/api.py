59db652fe2247b0ebf5558c942eeb0d4c90919d5
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2019-09-09 23:08:26 +0800

    update..

--- MachineLearning/TensorFlow/image-clssifier/tool/api.py
+++ MachineLearning/TensorFlow/image-clssifier/tool/api.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 # 2019-1-15
 
-import numpy
+import numpy as np
 import os
 
 
@@ -76,3 +76,55 @@ def pre_process(data, alpha=0.99, is_total=False):
             else:
                 ret[i, j] = [data[i, j], 1][data[i, j] == 0] / max_value * alpha
     return ret
+
+
+def predictionTopK(pdt, k):
+    """预测值中topk
+    @param pdt 预测结果，nupmy数组格式
+    @param k 前k个结果
+
+    @return topk结果，numpy数组格式
+    """
+    m, n = np.shape(pdt)
+    ret = []
+    for i in range(m):
+        curNums = pdt[i]
+        tmp = topK(curNums.tolist()[0], k)
+        ret.append(tmp)
+
+    return np.mat(ret)
+
+def topK(inputNums, k):
+    """获得数组中值前k大的索引
+    @param inputNums python列表一维
+    @param k 前k大
+
+    @param ret 前k大的索引
+    """
+    import copy
+    nums = copy.deepcopy(inputNums)
+    ret = []
+    for i in range(k):
+        tmpMaxIndex, tmpMaxVal = 0, float('-inf')
+        for index, val in enumerate(nums):
+            if tmpMaxVal < val:
+                tmpMaxVal = val
+                tmpMaxIndex = index 
+        nums[tmpMaxIndex] = float('-inf')
+        ret.append(tmpMaxIndex)
+
+    return ret 
+
+# test!
+# def topKTest():
+#     pdt = np.mat([[1,3,5,2,9,11,6, 10], [5,1,78,4,345,67,23,11]])
+#     k = 5
+
+#     ret = predictionTopK(pdt, k)
+#     print(ret)
+
+# topKTest()
+# """
+# [[5 7 4 6 2]
+#  [4 2 5 6 7]]
+#  """
--- MachineLearning/TensorFlow/image-clssifier/tool/test.py
+++ MachineLearning/TensorFlow/image-clssifier/tool/test.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 import os
+import api
 
 def test():
 	"""测试用"""

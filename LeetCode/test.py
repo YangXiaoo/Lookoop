e73a5161f348279a7b87d069464ba61c973d5c1e
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2019-09-09 21:16:50 +0800

    Update test.py

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -11,3 +11,12 @@ def test_strOps():
 	str1 = 'a'
 	str2 = 'c'
 	print(str2 - str1)
+
+try:
+	for n in gsearch1.cv_results_['mean_test_socre']:
+		print("[INFO] details of gsearch1.cv_results_:{}".format(n))
+except Exception as e:
+	print("[ERROR] error info:{}".format(str(e)))
+
+print("[INFO] best parameters:{}".format(gsearch1.best_params_))
+print("[INFO] best scores:{}".format(gsearch1.best_score_))

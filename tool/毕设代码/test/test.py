b5a986601c73c475c49aae47a8f041d76a285117
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2020-01-06 11:34:20 +0800

    Update test.py

--- tool/毕设代码/test/test.py
+++ tool/毕设代码/test/test.py
@@ -1,9 +1,23 @@
 # coding:utf-8
+import sys
+sys.path.append("../")
+
 import math
 import numpy as np 
 import datetime
 import os
 
+from util import io, tool
+
+dataFilePath = "../data/samples-data.data"
+labelsFilePath = "../data/samples-data-labels.data"
+
+def getTrainData():
+    """获取训练数据"""
+    X = io.getData(dataFilePath)
+    Y = io.getData(labelsFilePath)
+
+    return X, Y
 
 def foo():
     print(GLOBAL_VAR)
@@ -12,29 +26,6 @@ GLOBAL_VAR = "GLOBAL_VAR"
 
 foo()
 
-string = "hello world"
-
-def characterCount(string):
-    """统计词频"""
-    ret = {}
-    for c in string:
-        if c == ' ': continue
-        ret[c] = ret.get(c, 0) + 1
-
-    return ret
-
-def testCharacterCount():
-    """测试"""
-    string = "hello world"
-    ret = characterCount(string)
-
-    print(ret)
-
-def openSomething():
-    with open("record.txt") as f:
-        lines = f.readlines()
-        for line in lines:
-            pass # ?????
 class A():
     def __init__(self):
         self.data = []
@@ -44,7 +35,6 @@ class A():
 
 
 if __name__ == '__main__':
-    modelPathFormat = "../data/sarsa-2020-1-5.model"
-    print(os.path.basename(modelPathFormat))
-    print(os.path.dirname(modelPathFormat))
+    x,y = getTrainData()
+    print(len(x), len(y))
 

9213df69b6d254cc89d4b5a000dc983ea7cdc9b7
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2020-05-21 21:53:26 +0800

    Update algorithmsPractice-2020-5-15.py

--- AlgorithmsPractice/algorithmsPractice-2020-5-15.py
+++ AlgorithmsPractice/algorithmsPractice-2020-5-15.py
@@ -1,6 +1,8 @@
 # coding:utf-8
 # start: 2020-5-15
 # 剑指offer内容
+from queue import PriorityQueue, Queue
+import heapq
 
 class TreeNode(object):
     def __init__(self, val, left=None, right=None, parent=None):
@@ -173,7 +175,7 @@ def test_nextNode():
 
 #######################################
 # 09 用两个栈实现队列
-class Queue():
+class _Queue():
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
@@ -193,7 +195,7 @@ class Queue():
         return self.stack2.pop()
 
 def test_Queue():
-    queue = Queue()
+    queue = _Queue()
     print(queue.pop())
     queue.push(1)
     queue.push(2)
@@ -474,4 +476,436 @@ def test_subTree():
     ret = subTree(root, node)
     print(ret)
 
-test_subTree()
+# test_subTree()
+
+#######################################
+# 最长公共子字符串
+def getLCS(s1, s2):
+    dp = [[0 for _ in range(len(s2))] for _ in range(len(s1))]
+    maxLength = 0
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            if s1[i] == s2[j]:
+                if i == 0 and j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i-1][j-1] + 1
+
+            else:
+                dp[i][j] = 0
+
+            maxLength = max(maxLength, dp[i][j])
+
+    return maxLength
+
+def test_getLCS():
+    s1 = "acbcbcef"
+    s2 = "abcbced"
+
+    ret = getLCS(s1, s2)
+    print(ret)
+
+# test_getLCS()
+
+#######################################
+# 27 二叉树的镜像
+def mirrorTree(root):
+    if root == None: return 
+    if root.left == None and root.right == None: return 
+
+    root.left, root.right =  root.right, root.left 
+
+    if root.left:
+        mirrorTree(root.left)
+
+    if root.right:
+        mirrorTree(root.right)
+
+def invertTree(root):
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        if node:
+            node.left, node.right = node.right, node.left
+            stack.extend([node.left, node.right])
+    return root
+
+def test_mirrorTree():
+    root = getTree()
+    ret = mirrorTree(root)
+
+    printTreePreorder(root)
+
+def test_invertTree():
+    root = getTree()
+    ret = invertTree(root)
+    printTreePreorder(ret)
+
+# test_mirrorTree()
+# test_invertTree()
+
+#######################################
+# 28 对称的二叉树
+
+#######################################
+# 29 顺时针打印矩阵
+
+#######################################
+# 31 栈的压入弹出顺序
+def isPopOrder(push, pop):
+    pass
+
+#######################################
+# 32 从上到下打印二叉树
+def printTreeFromTop2Bottom(root):
+    stack = [root]
+    while stack:
+        curSize = len(stack)
+        tmp = []
+        for i in range(curSize):
+            cur = stack.pop()
+            print(cur.val)
+            if cur.left:
+                tmp.append(cur.left)
+            if cur.right:
+                tmp.append(cur.right)
+
+        stack = tmp[::-1]
+
+def test_printTreeFromTop2Bottom():
+    root = getTree()
+    printTreeFromTop2Bottom(root)
+
+# test_printTreeFromTop2Bottom()
+
+#######################################
+# 33 二叉搜索树的后续遍历序列
+def verifyBST(seq):
+    pass
+
+#######################################
+# 34 二叉树中和为某一值的路径 
+# LC-113(bbc), 114(bbd)
+def pathSum(root, sums):
+    pass
+
+#######################################
+# 35 复杂链表的复制
+# LC-138
+def copyLinkedList(head):
+    insertNewNode(head)
+    linkedRandomNode(head)
+    newHead = splitLinkedList(head)
+
+#######################################
+# 36 二叉搜索树与双向链表
+def convertBSTToLinkedList(root):
+    pass
+
+#######################################
+# 38 字符串的排序
+def stringPermutation(s):
+    pass
+
+#######################################
+# 39 数组中出现次数超过一半的数字
+
+#######################################
+# 40 最小的K个数
+# 23 merge k sorted list
+def minK(nums, k):
+    q = PriorityQueue()
+    for n in nums:
+        q.put(tuple([n, n]))
+
+    ret = []
+    for j in range(k):
+        ret.append(q.get()[-1])
+
+    return ret 
+
+def test_minK():
+    nums = [4,5,1,6,2,7,3,8]
+    k = 4
+    ret = minK(nums, k)
+    print(ret)
+
+# test_minK()
+#######################################
+# 41 数据流中的中位数
+class medianNumber():
+    """最小堆与最大堆组合"""
+    def __init__(self):
+        self.minStack = []
+        self.maxStack = []
+
+    def peekMin(self):
+        return self.minStack[0]
+
+    def popMin(self):
+        return heapq.heappop(self.minStack)
+
+    def peekMax(self):
+        return -self.maxStack[0]
+
+    def popMax(self):
+        return heapq.heappop(self.maxStack)
+
+    def pushMin(self, x):
+        heapq.heappush(self.minStack, x)
+
+    def pushMax(self, x):
+        heapq.heappush(self.maxStack, -x)
+
+    def insert(self, num):
+        """插入数据"""
+        if (len(self.minStack) + len(self.maxStack)) & 1 == 0:
+            if len(self.maxStack) != 0 and self.peekMax() > num:
+                tmp = self.popMax()
+                self.pushMax(num)
+                num = tmp
+            self.pushMin(num)
+        else:
+            if len(self.minStack) != 0 and self.peekMin() < num:
+                tmp = self.popMin()
+                self.pushMin(num)
+                num = tmp 
+            self.pushMax(num)
+
+    def getMedian(self):
+        """偶数长度则取中间两个数的平均值，奇数长度则取中间数"""
+        numsSize = len(self.minStack) + len(self.maxStack)
+
+        if numsSize == 0:
+            return '-1'
+
+        median = 0
+        if numsSize & 1 == 0:
+            median = (self.peekMin() + self.peekMax()) / 2.0
+        else:
+            median = self.peekMin()
+
+        return median
+
+def test_medianNumber():
+    nums = [1,2,3,4,5,6,7,8,9,10]
+    q = medianNumber()
+    tmp = []
+    for n in nums:
+        q.insert(n)
+        tmp.append(n)
+        median = q.getMedian()
+        print("data: {}, median: {}".format(tmp, median))
+
+# test_medianNumber()
+
+#######################################
+# 42 连续子数组的最大和(最长递增数组)
+# LC-54
+def findGreastSumOfSubArray(nums):
+    for i in range(1, len(nums)):
+        if nums[i-1] > 0:
+            nums[i] +=  nums[i-1]
+    return max(nums)
+
+def test_findGreastSumOfSubArray():
+    nums = [1,-2,3,10,-4,7,2,-5]
+    ret = findGreastSumOfSubArray(nums)
+    print(ret)
+
+# test_findGreastSumOfSubArray()
+
+#######################################
+# 45 把数组排成最小的数
+def getMinNumber(nums):
+    sNums = [str(n) for n in nums]
+    for i in range(1, len(nums)):
+        leftComb = sNums[i-1] + sNums[i]
+        rightComb = sNums[i] + sNums[i-1]
+        if leftComb < rightComb:
+            sNums[i] = leftComb
+        else:
+            sNums[i] = rightComb
+
+    return sNums[-1]
+
+def test_getMinNumber():
+    nums = [3, 32, 321]
+    ret = getMinNumber(nums)
+    print(ret)
+
+# test_getMinNumber()
+
+#######################################
+# 46 把数字翻译成字符串
+# LC-91
+def translateNumbers2String(num):
+    if num < 0: return 0
+    strNum = str(num)
+    dp = [1] + [0 for _ in strNum]
+    dp[1] = 1
+    for i in range(1, len(strNum)):
+        dp[i+1] += dp[i]
+        if strNum[i-1] == '0': continue
+        curComb = strNum[i-1] + strNum[i]
+        if int(curComb) < 26:
+            dp[i+1] += dp[i-1]
+    return dp[-1]
+
+def isValidAlpha(s1, s2):
+    if int(s1+s2) < 26:
+        return True
+
+def translateNumbers2String_2(num):
+    def helper(s):
+        nonlocal ret
+        if len(s) == 0: ret += 1
+
+        if len(s) > 0:
+            helper(s[1:])
+
+        if len(s) > 1:
+            if isValidAlpha(s[0], s[1]):
+                helper(s[2:])
+    strNum = str(num)
+    ret = 0
+    if len(strNum) == 0:
+        return ret 
+    helper(strNum)
+
+    return ret 
+
+def test_translateNumbers2String():
+    num = 12258
+    ret = translateNumbers2String_2(num)
+    print(ret)
+
+# test_translateNumbers2String()
+
+#######################################
+# 47 礼物的最大价值
+def getMaxValueOfPresent(values):
+    row, col = len(values), len(values[0])
+    dp = [[0 for _ in range(col)] for _ in range(row)]
+    dp[0][0] = values[0][0]
+    
+    for r in range(1, row):
+        dp[r][0] = dp[r-1][0] + values[r][0]
+    for c in range(col):
+        dp[0][c] = dp[0][c-1] + values[0][c]
+
+    for r in range(1, row):
+        for c in range(1, col):
+            dp[r][c] = values[r][c] + max(dp[r-1][c], dp[r][c-1])
+
+    return dp[-1][-1]
+
+def test_getMaxValueOfPresent():
+    values = [
+        [1,10,3,8],
+        [12,2,9,6],
+        [5,7,4,11],
+        [3,7,16,5]
+    ]
+
+    ret = getMaxValueOfPresent(values)
+    print(ret)
+
+# test_getMaxValueOfPresent()
+
+#######################################
+# 48 最长不含重复字符串的子字符串
+def longestSubstringWithoutDuplication(s):
+    container = [-1 for i in range(127)]
+    preIndex = -1
+    maxLength = 0
+    for i, c in enumerate(s):
+        cur = ord(c)
+        oldIndex = container[cur]
+        container[cur] = i
+        if oldIndex != -1 and oldIndex > preIndex:
+            preIndex = oldIndex
+        
+        maxLength = max(maxLength, i - preIndex)
+
+    return maxLength
+
+def test_longestSubstringWithoutDuplication():
+    s = "arabcacfr"
+    ret = longestSubstringWithoutDuplication(s)
+    print(ret)
+
+# test_longestSubstringWithoutDuplication()
+
+#######################################
+# 49 丑数
+def uglyNumber(index):
+    """只包含2,3,5的数称为丑数，获得第index个顺序的丑数"""
+    pass
+
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
+#######################################
